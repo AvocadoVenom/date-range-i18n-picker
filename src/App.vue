23734058560f9f167ac428f5ceb42a054138c969
Adrien MIQUEL <admiquel94@gmail.com>
2020-06-20 15:01:20 +0200

    Add DateRangePicker component

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,15 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <DateRangePicker />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import DateRangePicker from './components/DateRangePicker.vue';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld,
-  },
+  components: { DateRangePicker },
 };
 </script>
 
@@ -23,6 +20,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 60px 30% 0 30%;
 }
 </style>
---
+++ src/components/DateRangePicker.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="container">
+    <label class="label" for="from">From</label>
+    <div class="container-inline">
+      <select name="from-month" id="dp-from-month">
+        <option :value="null">Pick a month</option>
+      </select>
+      <select name="from-year" id="dp-from-year">
+        <option :value="null">Pick a year</option>
+      </select>
+    </div>
+    <label class="label" for="to">To</label>
+    <div class="container-inline">
+      <select name="to-month" id="dp-to-month">
+        <option :value="null">Pick a month</option>
+      </select>
+      <select name="to-year" id="dp-to-year">
+        <option :value="null">Pick a year</option>
+      </select>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "DateRangePicker"
+};
+</script>
+
+<style scoped lang="scss">
+.container {
+  width: 100%;
+  display: flex;
+  align-content: stretch;
+  align-items: flex-start;
+  flex-direction: column;
+
+  > * {
+    margin: 0 4px;
+  }
+}
+
+.container-inline {
+  display: flex;
+}
+
+select {
+  margin-right: 8px;
+  min-width: 150px;
+  padding: 4px;
+  border-radius: 4px;
+}
+
+.label {
+  margin: 8px 0;
+}
+</style>
